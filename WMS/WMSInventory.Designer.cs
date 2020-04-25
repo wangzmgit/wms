f4dd58409b21ec054f6225c556d047a14799b440
wzm <18105387190@163.com>
2020-04-25 20:04:05 +0800

    基本实现查询功能

--- WMS/WMSInventory.Designer.cs
+++ WMS/WMSInventory.Designer.cs
@@ -36,7 +36,13 @@
             this.supplier = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.entry = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.remarks = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvInventory)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvInventory
@@ -50,8 +56,7 @@
             this.supplier,
             this.entry,
             this.remarks});
-            this.dgvInventory.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvInventory.Location = new System.Drawing.Point(0, 0);
+            this.dgvInventory.Location = new System.Drawing.Point(0, 97);
             this.dgvInventory.Name = "dgvInventory";
             this.dgvInventory.RowTemplate.Height = 27;
             this.dgvInventory.Size = new System.Drawing.Size(1082, 533);
@@ -104,11 +109,64 @@
             this.remarks.Name = "remarks";
             this.remarks.Width = 150;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1082, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "查找";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(258, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "产品名称";
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(353, 38);
+            this.textName.Multiline = true;
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(244, 32);
+            this.textName.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(649, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "查找";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(793, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 32);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "显示全部";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // WMSInventory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1082, 533);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.dgvInventory);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -116,6 +174,8 @@
             this.Text = "库存信息";
             this.Load += new System.EventHandler(this.WMSInventory_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvInventory)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -130,5 +190,10 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn supplier;
         private System.Windows.Forms.DataGridViewTextBoxColumn entry;
         private System.Windows.Forms.DataGridViewTextBoxColumn remarks;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- WMS/WMSInventory.cs
+++ WMS/WMSInventory.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WMS
 {
@@ -24,6 +25,31 @@ namespace WMS
             dgvInventory.DataSource = dtGradeList;
         }
 
-       
+        /// <summary>
+        /// 查找按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textName.Text.Trim();
+            string sql = "select productID,name,stock,unit,supplier,entry,remarks from Inventory where name Like @name";
+            if(!string.IsNullOrEmpty(name))
+            {
+                SqlParameter[] paras =
+                {
+                    new SqlParameter("@name","%"+name+"%")
+                };
+                DataTable dataTable = sqlHelper.GetDataTable(sql, paras);
+                dgvInventory.DataSource = dataTable;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sql = "select productID,name,stock,unit,supplier,entry,remarks from Inventory";
+            DataTable dtGradeList = sqlHelper.GetDataTable(sql);
+            dgvInventory.DataSource = dtGradeList;
+        }
     }
 }
